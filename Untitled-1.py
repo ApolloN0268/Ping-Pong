f166cb6241cf70cbc10e10feef129dc16eefff1d
ApolloN0268 <102855538+ApolloN0268@users.noreply.github.com>
2022-04-09 13:32:22 +0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -14,12 +14,45 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y > 5:
-            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
+        
+
+
+main_win = display.set_mode((700,500))
+display.set_caption("Шутер")
+background = transform.scale(image.load("Dekoratsii-starogo-goroda-na-kinostudii-Mosfilm-700x500.jpg"),(700,500))
+sprite1 = Player('Без имени.png', 30,30,65,120,5)
+sprite2 = Player('Без имени.png', 500,400,65,120,5)
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+            
+
+
+    if finish != True:          
+        main_win.blit(background,(0,0))
+        sprite1.update_l()
+        sprite2.update_r()
+        sprite1.reset()
+        sprite2.reset()
+
+    clock.tick(FPS)
+    display.update()
+
 
         
         

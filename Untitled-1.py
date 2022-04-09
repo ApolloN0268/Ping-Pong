c3179bca885312eddd019bdb4c109c03e69723bc
ApolloN0268 <102855538+ApolloN0268@users.noreply.github.com>
2022-04-09 12:56:26 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,w,h,player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(w,h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        main_win.blit(self.image,(self.rect.x,self.rect.y))
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y > 5:
+            self.rect.y -= self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        
+        
